9972a9fc486b77191e9f1bd04cb2a5d30864a04c
BronelL87 <bronellazar@gmail.com>
2024-11-15 16:54:06 -0800

    DOMMMMM

---
+++ README.md
@@ -0,0 +1,15 @@
+# Challenge - Manipulating The DOM
+
+**Name**: Bronel Lazar
+
+**Date**: 11/12/2024
+
+**Last Revised**: 11/15/2024
+
+**Description**: I altered 10 things using wickedcss on a random page using javascript
+
+### Peer Review
+
+**Reviewer**: 
+
+**Reviewer Notes**: 
--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,13 @@ let brockCard = document.getElementById("brockCard");
 let domImg = document.getElementById("domImg");
 let awaitChange1 = document.getElementById("awaitChange1");
 let awaitChange2 = document.getElementById("awaitChange2");
-let awaitChange3 = document.getElementById("awaitChange3");
+
 let awaitChange4 = document.getElementById("awaitChange4");
 let awaitChange5 = document.getElementById("awaitChange5");
 let awaitChange6 = document.getElementById("awaitChange6");
 let awaitChange7 = document.getElementById("awaitChange7");
 let awaitChange8 = document.getElementById("awaitChange8");
-
+let theBody = document.getElementById("theBody");
 
 let domBtn = document.getElementById("domBtn");
 let cardBtn = document.getElementById("cardBtn");
@@ -36,11 +36,39 @@ cardBtn.addEventListener("click", function(){
 });
 
 changeBtn1.addEventListener("click", function(){
-    awaitChange1.innerHTML = "I am now a titan."
-    awaitChange1.className = "change1 " + "floater"
+    awaitChange1.innerHTML = "I am now a titan.";
+    awaitChange1.className = "change1 " + "floater";
 });
 
 changeBtn2.addEventListener("click", function(){
-    awaitChange2.innerHTML = "I CAN'T STOP SPINNING!"
-    awaitChange2.className = "change2 " + "spinner"
+    awaitChange2.innerHTML = "I CAN'T STOP SPINNING!";
+    awaitChange2.className = "change2 " + "spinner";
+});
+
+changeBtn3.addEventListener("click", function(){
+    theBody.className = "bodyChange";
+});
+
+changeBtn4.addEventListener("click", function(){
+awaitChange4.innerHTML = "This is fun";
+awaitChange4.className = "textChange " + "pulse";
+});
+
+changeBtn5.addEventListener("click", function(){
+awaitChange5.innerHTML = " I am a different item changing, trust";
+awaitChange5.className = "textChange " + "barrelRoll";
+});
+
+changeBtn6.addEventListener("click", function(){
+awaitChange6.innerHTML = "Wiggle wiggle";
+awaitChange6.className = "textChange " + "wiggle";
+});
+
+changeBtn7.addEventListener("click", function(){
+awaitChange7.innerHTML = "Im so scared im shaking in my boots";
+awaitChange7.className = "textChange " + "shake";
+});
+changeBtn8.addEventListener("click", function(){
+awaitChange8.innerHTML = "Don't forget your alt tags kids";
+awaitChange8.className = "textChange " + "zoomer" 
 });
